7ae0f725ae97324fea9331481873c73f71cf7b38
lijiahui <lijiahui@accurmedicine.com>
2017-01-18 18:04:53 +0800

    homepage

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,53 +1,140 @@
 <template>
 <div class="home">
   <div class="swipe-warrper">
-    <div class="address">
-      <span>送至：</span>
-      <span>深圳职业技术学院北校区A座</span>
-      <i class="icon-drop_down"></i>
-    </div>
-    <div class="seach">
-      <p></p>
-      <span>搜索</span>
+    <div class="tool">
+      <div class="address">
+        <span>送至：</span>
+        <span class="adde">深圳职业技术学院北校区A座</span>
+        <i class="icon-drop_down"></i>
+      </div>
+      <div class="search">
+        <i class="icon-search"></i>
+        <span>搜索</span>
+      </div>
+    </div>  
+  </div>
+  <div class="species">
+    <div class="species-warrper" v-for="specie in species" :href="specie.url">
+      <i :class="specie.iconClass"></i>
+      <p v-text="specie.text"></p>
     </div>
   </div>
-  <div>I am home</div>
+  <split></split>
 </div>
 </template>
 
 <script>
+import split from 'components/split/split';
+export default{
+    data() {
+        return {
+            species: [
+              {
+                iconClass: 'icon-brand',
+                text: '品牌馆',
+                url: '#/home'
+              },
+              {
+                iconClass: 'icon-coffee',
+                text: '下午茶',
+                url: '#/home'
+              },
+              {
+                iconClass: 'icon-cake',
+                text: '鲜花蛋糕',
+                url: '#/home'
+              },
+              {
+                iconClass: 'icon-fresh',
+                text: '冰冻新鲜',
+                url: '#/home'
+              },
+              {
+                iconClass: 'icon-hamburg',
+                text: '美味快餐',
+                url: '#/home'
+              }
+            ]
+        };
+    },
+    components: {
+      split
+    }
+};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+ @import "../../common/stylus/mixin";
   .swipe-warrper
     width: 100%
     height: 160px
     background-color: red
-    .address
-      overflow: hidden
-      text-overflow:ellipsis
-      white-space: nowrap
+
+    .tool
       position: absolute
       top: 32px
-      left: 91px
-      right: 91px
-      border-radius: 25px
+      left: 90px
+      right: 10px
       height: 24px
-      background-color: rgba(7, 17, 27, 0.3)
-      font-size: 14px
-      line-height: 24px
-      color: #fff
       font-weight: 700
-      padding: 0 20px 0 12px
-      .icon-drop_down
-        width: 24px
-        text-align: center
-        display: block
-        line-height: 24px
+
+      .address
         position: absolute
-        right: 0
-        top: 0
-        font-size: 6px
+        left: 0
+        right: 82px
+        font-size: 14px
+        overflow: hidden
+        text-overflow:ellipsis
+        white-space: nowrap
+        border-radius: 25px
+        background-color: rgba(7, 17, 27, 0.3)
+        line-height: 24px
         color: #fff
+        padding: 0 10px 0 16px
+
+        .icon-drop_down
+          width: 24px
+          text-align: center
+          display: block
+          line-height: 24px
+          position: absolute
+          top: 0
+          right: 0
+          font-size: 10px
+          color: #fff
+
+      .search
+        width: 74px
+        height: 24px
+        border-radius: 25px
+        background-color: rgba(7, 17, 27, 0.3)
+        line-height: 24px
+        color: #fff
+        font-size: 14px
+        text-align: center
+        position: absolute
+        top: 0
+        right: 0
+
+  .species
+    display: flex
+    box-sizing: border-box
+    width: 100%
+    padding: 22px 14px
+    height: 108px
+    .species-warrper
+      flex: 1
+      display: inline-block
+      text-align: center
+      i
+        font-size: 44px
+        line-height: 44px
+        color: #ff5656
+      p
+        padding-top: 10px
+        font-size: 12px
+        line-height: 12x
+
+
 
 </style>
